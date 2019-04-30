174c2da8140904bebb6983a2daff4fdcb3448d43
cwlowder <clowder2@illinois.edu>
2019-04-30 12:13:04 -0500

    Removed files

--- npmrec/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class NpmrecConfig(AppConfig):
-    name = 'npmrec'
--- npmrec/forms.py
+++
@@ -1,5 +0,0 @@
-from django import forms
-
-class UploadFileForm(forms.Form):
-    file = forms.FileField()
-    #allow_save = forms.BooleanField()
